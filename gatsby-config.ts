0d6e3e70dff0ffa41ba0b00ba777cfce79091924
wandriputra <wandriputra@gmail.com>
2022-04-09 15:05:18 +0700

    add tailwind css

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,23 +1,26 @@
 import type { GatsbyConfig } from "gatsby";
+import path from "path";
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `landing-page-pscs`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "aaa"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }]
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-sass", {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        "trackingId": "aaa"
+      }
+    }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": path.join(__dirname, `src`, `images`),
+      },
+    }]
 };
 
 export default config;
