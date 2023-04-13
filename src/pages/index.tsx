4c0e618313f60a43c2df915c0e5e29713481dfcb
wandriputra <wandriputra@gmail.com>
2023-04-13 11:35:20 +0700

    update landing page for sale

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,30 +10,16 @@ const IndexPage = () => {
     <>
       <Helmet>
         <link rel="icon" href={Logo} />
+        <title>PSCP Cilacap | Laskar Nusakambangan</title>
+        <meta name="description" content='Website resmi Klub PSCS Cilacap. PSCS Cilacap (Persatuan Sepak Bola Cilacap dan Sekitarnya) merupakan tim sepakbola yang berbasis di Kabupaten Cilacap, Jawa Tengah, Indonesia. Tim ini memiliki julukan sebagai tim Hiu Pantai Selatan juga dikenal dengan sebutan "Laskar Nusakambangan". PSCS Cilacap bermain di Liga 2. Di kompetisi musim 2017 merupakan musim dimana mereka terdegradasi ke Liga 3. Tahun 2019 mereka promosi kembali ke Liga 2 dan masih bertahan hingga saat ini.' />
       </Helmet>
       <div className="flex flex-col justify-center items-center h-screen">
-        <div className="pb-16">
-          <img src={Logo} width="300px" className="mx-auto" alt="Persatuan Sepak Bola Cilacap dan Sekitarnya, commonly known as PSCS Cilacap is a football club in Indonesia, based in Cilacap Regency, Central Java. In 2018, the club was promoted to the Liga 2 after became the runner-up of 2018 Liga 3." />
+        <div className="">
+          <h1>Domain For Sale</h1>
         </div>
-        <div className="grid xl:grid-cols-4 grid-cols-2  text-center place-items-center text-gray-500">
-          <a href="https://www.facebook.com/PSCS-Cilacap-Official-1039210012908141"
-            className="inline-flex ml-3 hover:text-[#020a6d]">
-            <FaFacebook className="mr-1" /> PSCS Cilacap Official
-          </a>
-          <a href="https://twitter.com/PSCS_Cilacap"
-            className="inline-flex ml-3 hover:text-[#020a6d]">
-            <FaTwitterSquare className="mr-1" /> @PSCS_Cilacap
-          </a>
-          <a href="https://www.instagram.com/pscscilacap.official/"
-            className="inline-flex ml-3 hover:text-[#020a6d]">
-            <FaInstagram className="mr-1" /> pscscilacap.official
-          </a>
-          <a href="https://www.youtube.com/channel/UCQVYQZotYXO0Uv5C_AF08XA?sub_confirmation=1"
-            className="inline-flex ml-3 hover:text-[#020a6d]">
-            <FaYoutube className="mr-1" /> PSCS Cilacap Official
-          </a>
+        <div className="pt-16">
+          contact: <a href="mailto:wandriputra@gmail.com"> wandriputra@gmail.com</a>
         </div>
-
       </div>
     </>
   )
